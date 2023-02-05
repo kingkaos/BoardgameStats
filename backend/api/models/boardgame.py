7b744e371b20835031234893f25025b9a9c66aa4
kingkaos <da.cebulla@gmail.com>
2023-02-05 11:39:00 +0100

    feat: add boardgame type to boardgame model

--- backend/api/models/boardgame.py
+++ backend/api/models/boardgame.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
 
 
 class Boardgame(models.Model):
+
+    class BoardgameType(models.TextChoices):
+        COMPETITIVE = 'COMP', gettext_lazy('kompetitiv')
+        COOPERATIVE = 'COOP', gettext_lazy('kooperativ')
+
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    # type of game solitar, coop, compet
+    type = models.CharField(
+        max_length=4,
+        choices=BoardgameType.choices,
+        default=BoardgameType.COMPETITIVE
+    )
 
     def __str__(self):
         return self.title
