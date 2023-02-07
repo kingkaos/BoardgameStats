9baa45fb1333f573480abc8df26d9e365c9be918
kingkaos <da.cebulla@gmail.com>
2023-02-07 21:35:19 +0100

    feat!: extend boardgame class

--- backend/api/models/boardgame.py
+++ backend/api/models/boardgame.py
@@ -8,13 +8,33 @@ class Boardgame(models.Model):
         COMPETITIVE = 'COMP', gettext_lazy('kompetitiv')
         COOPERATIVE = 'COOP', gettext_lazy('kooperativ')
 
+    class BoardgameKind(models.TextChoices):
+        STANDARD = 'STD', gettext_lazy('')
+        FAMILY = 'FAM', gettext_lazy('Familie')
+        KIDS = 'KID', gettext_lazy('Kinder')
+        ADULT = 'ADU', gettext_lazy('Erwachsene')
+        EXPERT = 'EXP', gettext_lazy('Experten')
+
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    # type of game solitar, coop, compet
+    is_expansion = models.BooleanField(default=False)
+
+    min_player = models.PositiveSmallIntegerField(blank=True, null=True)
+    max_player = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    min_duration = models.PositiveSmallIntegerField(blank=True, null=True)
+    max_duration = models.PositiveSmallIntegerField(blank=True, null=True)
+    # game_variants = models.JSONField(null=True)
+    # type of game coop or compet
     type = models.CharField(
         max_length=4,
         choices=BoardgameType.choices,
         default=BoardgameType.COMPETITIVE
+    )
+    kind = models.CharField(
+        max_length=3,
+        choices=BoardgameKind.choices,
+        default=BoardgameKind.STANDARD
     )
 
     def __str__(self):
