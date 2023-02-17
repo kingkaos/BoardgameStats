b2f7403f5efcfb13ee7ce4a092c810978a682b8a
kingkaos <da.cebulla@gmail.com>
2023-02-17 23:28:58 +0100

    feat: obtain database settings from .env file

--- backend/backend/settings/development.py
+++ backend/backend/settings/development.py
@@ -1,12 +1,41 @@
+import os
+
+from decouple import config
+
 from .base import *
+
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# database settings are read from .env file. If no .env file is present, django
+# default value (sqlite3 database) are used.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config(
+            'SQL_ENGINE', 
+            'django.db.backends.sqlite3'
+        ),
+        'NAME': config(
+            'SQL_DATABASE',
+            BASE_DIR / 'db.sqlite3'
+        ),
+        'USER': config(
+            'SQL_USER',
+            ''
+        ),
+        'PASSWORD': config(
+            'SQL_PASSWORD',
+            ''
+        ),
+        'HOST': config(
+            'SQL_HOST',
+            ''
+        ),
+        'PORT': config(
+            'SQL_PORT',
+            ''
+        )
     }
 }
